6baf070d8f8879d9670b9f8cec951ca4ff7d2fdd
lidongze0629 <9546726@qq.com>
2022-10-23 20:06:27 +0800

    finished sleep

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[]) {
+  if (argc <= 1) {
+    fprintf(2, "sleep: params error\n");
+  }
+
+  int seconds = atoi(argv[1]);
+  sleep(seconds);
+  exit(0);
+}
