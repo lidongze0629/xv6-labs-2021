fcacbcfa79701da2c7b1f7ef8aa0e7406d5eefc9
lidongze0629 <9546726@qq.com>
2022-11-13 17:26:52 +0800

    finished trace syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -292,6 +292,9 @@ fork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  // lab: copy tracemark from parent to chind.
+  np->tracemask = p->tracemask;
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+// lab
+uint64
+sys_trace(void)
+{
+  int mask;
+
+  if (argint(0, &mask) < 0)
+    return -1;
+  myproc()->tracemask = mask;
+  return 0;
+}
